6d6675665876ba4866dc85cb69a057e852107df3
soniaccodes <soniaccodes@gmail.com>
2024-12-07 11:46:45 -0500

    Convert favoriteSongs array to a list, then transfer songs from favoriteSongs list to desertIslandPlaylist ArrayList

---
+++ src/Playlist.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Playlist {
+    public static void main(String[] args) {
+
+        //declare array
+        String [] favoriteSongs;
+
+        // String array of favorite songs
+        favoriteSongs = new String[]{"Sabrina Carpenter - Taste", "Sabrina Carpenter - Bed Chem","Sabrina Carpenter - Please Please Please","Sabrina Carpenter - Espresso","Bad Bunny - Titi Me Pregunto", "Bad Bunny - Ojitos Lindos","Ariana Grande - Eternal Sunshine","Ariana Grande - Bye", "Ariana Grande - Imperfect For You", "Ariana Grande - I Wish I Hated You"};
+
+        // prints first 3 songs in favorite songs array
+        for(int i = 0; i < 3; i++){
+            System.out.println(favoriteSongs[i]);
+        }
+
+        // creates new String ArrayList
+        ArrayList<String> desertIslandPlaylist = new ArrayList<>();
+
+        // adds songs to ArrayList desertIslandPlaylist
+        desertIslandPlaylist.add("The Cranberries - Linger");
+        desertIslandPlaylist.add("Mazzy Star - Fade Into You");
+        desertIslandPlaylist.add("The Marias - No One Noticed");
+        desertIslandPlaylist.add("The Marias - Heavy");
+        desertIslandPlaylist.add("The Marias - Carino");
+
+        System.out.println(desertIslandPlaylist);
+
+        // convert array to a list (to transfer to ArrayList)
+        // Add list (favoriteSongs) to ArrayList using addAll() method
+        desertIslandPlaylist.addAll(Arrays.asList(favoriteSongs));
+
+        System.out.println(desertIslandPlaylist);
+
+    }
+}
